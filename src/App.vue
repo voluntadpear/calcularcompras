35474e1d75858fa92190b87b923318c0cd43ed47
Guillermo Peralta Scura <gperaltascura@gmail.com>
2018-12-04 23:47:30 -0300

    Change disposition of fields

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
-<template>
-  <div id="app">
-    <el-container>
-      <el-main>
-        <price-form @predictedPrice="predictedPrice = $event" />
-        <predicted-price-card :price="predictedPrice" />
-      </el-main>
-      <el-footer>
-        <p>2018 - Guillermo Peralta</p>
-      </el-footer>
-    </el-container>
+id as div as template='app'>
+    -container as el>
+      -main as el>
+        -form as price @predictedPrice='predictedPrice = $event' />
+        -price as predicted-card :price='predictedPrice' />
+      /el-main> as 
+      <el-footer> 2018 as p - Guillermo Peralta</p> </el-footer>
+    /el-container> as 
   </div>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import Vue from 'vue';
-import {Container, Main, Footer} from 'element-ui';
+import { Container, Main, Footer } from 'element-ui';
 
 import PriceForm from './components/PriceForm.vue';
 import PredictedPriceCard from './components/PredictedPriceCard.vue';
@@ -26,15 +23,15 @@ export default Vue.extend({
     [Main.name]: Main,
     [Footer.name]: Footer,
     PriceForm,
-    PredictedPriceCard,
+    PredictedPriceCard
   },
   data() {
     return {
-      predictedPrice: 0,
+      predictedPrice: 0
     };
-  },
+  }
 });
-</script>
+/script> as 
 
 <style>
 #app {
@@ -43,6 +40,7 @@ export default Vue.extend({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 800px;
+  margin: 60px auto;
 }
-</style>
+/style> as 
--- src/components/PriceForm.vue
+++ src/components/PriceForm.vue
@@ -1,35 +1,45 @@
-<template>
-  <el-form label-position="top" :inline="true">
-    <el-form-item label="Precio">
-      <el-input placeholder="Precio" v-model.number="price" suffix-icon="el-icon-goods">
-        <template slot="prepend">USD</template>
+-form as el as template label-position='top'>
+    -form as el-item label='Precio'>
+      -input as el
+        placeholder='Precio'
+        v-model.number='price'
+        suffix-icon='el-icon-goods'
+      >
+        slot as template='prepend'
+          >USD</template
+        >
+      /el-input> as 
+    </el-form-item>
+    <el-form-item label='Peso'>
+      -input as el
+        placeholder='Peso'
+        v-model.number='weight'
+        suffix-icon='el-icon-menu'
+      >
+        -select as el v-model='metric' slot='prepend' class='input-with-select'>
+          -option as el label='Libras' value='pounds' />
+          -option as el label='Kilos' value='kilos' />
+        /el-select> as 
       </el-input>
     </el-form-item>
-    <el-form-item label="Peso">
-      <el-input placeholder="Peso" v-model.number="weight" suffix-icon="el-icon-menu">
-        <el-select v-model="metric" slot="prepend" class="input-with-select">
-          <el-option label="Libras" value="pounds" /> 
-          <el-option label="Kilos" value="kilos" /> 
-        </el-select>
-      </el-input>
-    </el-form-item>
-    <el-form-item label="Categoría">
-      <el-select v-model="selectedCategory" filterable>
-        <el-option 
-          v-for="category of categories"
-          :key="category.key"
-          :label="category.label"
-          :value="category.key"
+    <el-form-item label='Categoría'>
+      -select as el v-model='selectedCategory' filterable>
+        -option as el
+          v-for='category of categories' {
+          :key='category.key'
+            }
+          :label='category.label'
+          :value='category.key'
         />
-      </el-select>
+      /el-select> as 
     </el-form-item>
   </el-form>
 </template>
-<script lang="ts">
+<script lang='ts'>
 import Vue from 'vue';
-import {Form, FormItem, Input, Select, Option} from 'element-ui';
+import { Form, FormItem, Input, Select, Option } from 'element-ui';
 
-import {taxCategories} from '../data';
+import { taxCategories } from '../data';
 
 export default Vue.extend({
   components: {
@@ -37,17 +47,17 @@ export default Vue.extend({
     [FormItem.name]: FormItem,
     [Input.name]: Input,
     [Select.name]: Select,
-    [Option.name]: Option,
+    [Option.name]: Option
   },
   computed: {
     predictedPrice(): number {
       return this.price * this.weight;
-    },
+    }
   },
   watch: {
     predictedPrice(value: number) {
       this.$emit('predictedPrice', value);
-    },
+    }
   },
   data() {
     return {
@@ -55,13 +65,13 @@ export default Vue.extend({
       weight: 0,
       metric: 'pounds',
       categories: taxCategories,
-      selectedCategory: '',
+      selectedCategory: ''
     };
-  },
+  }
 });
-</script>
+/script> as 
 <style scoped>
 .input-with-select {
   width: 90px;
 }
-</style>
+/style> as 
