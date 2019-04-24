da5f9d76720f1b99b3a28a336bb6d589d8b2136a
Guillermo Peralta Scura <gperaltascura@gmail.com>
2019-04-23 20:01:49 -0400

    Revert "Preload all kind of assets"
    This reverts commit 3b66c9a4da2b3f59be42164444e901956b69b1f8.

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-  chainWebpack: config => {
-    config.plugin('preload').tap(options => {
-      options[0].as = (entry) => {
-        if (/\.css$/.test(entry)) return 'style';
-        if (/\.woff$/.test(entry)) return 'font';
-        if (/\.png$/.test(entry)) return 'image';
-        return 'script';
-      }
-      options[0].include = 'allAssets'
-      return options
-    })
-  },
   pwa: {
     themeColor: "#f5f7fa"
   }
